9c0e6c6110384a8552cfcd50fcf18a94c8b6461c
Windsor Schmidt <windsor.schmidt@gmail.com>
2020-03-25 15:36:47 -0700

    work better on RPi, and plot in terminal without matplotlib

--- mso7034b-scpi-capture.py
+++ mso7034b-scpi-capture.py
@@ -1,11 +1,20 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 # About:
 #
 # Remotely configure MSO7034B oscilloscope and capture one-shot pulse data.
-# Requires Python VISA packages to access scope using socket/SCPI interface:
+# Requires Python VISA packages to access scope using socket/SCPI interface.
 #
-#   sudo pip3 install pyvisa pyvisa-py
+# Connection parameters are read from environment variables set with, e.g.:
+#
+#   export MSO7034B_HOST=192.168.0.243
+#   export MSO7034B_PORT=5025
+#
+# Prerequisites on Raspberry Pi 4:
+#
+#   sudo apt-get install python3-pyvisa python3-pyvisa-py
+#   sudo apt-get install python3-numpy python3-pip gnuplot
+#   sudo pip3 install termplotlib
 #
 # The term "guide" herein refers to the document:
 #
@@ -13,69 +22,55 @@
 #   https://www.keysight.com/upload/cmc_upload/All/7000B_series_prog_guide.pdf
 
 import os
+import sys
 import subprocess as proc
-import numpy as np
-# import matplotlib
-# matplotlib.rcParams['font.sans-serif'] = ['Go', 'sans-serif']
-import matplotlib.pyplot as plt
 import pyvisa
+import numpy as np
+import termplotlib as tpl
 
-# Read SCPI socket parameters from environment variables set with, e.g.:
-#
-# export MSO7034B_IP=n.n.n.n
-# export MSO7034B_PORT=nnnn
-#
-IP = os.getenv('MSO7034B_HOST')
-PORT = os.getenv('MSO7034B_PORT')
+HOST = os.getenv('MSO7034B_HOST', '192.168.0.243')
+PORT = os.getenv('MSO7034B_PORT', '5025')
 
-print('connecting to scope')
+print('connecting to scope at {}:{}'.format(HOST, PORT))
 rm = pyvisa.ResourceManager()
-scope = rm.open_resource('TCPIP::{}::{}::SOCKET'.format(IP, PORT))
+scope = rm.open_resource('TCPIP::{}::{}::SOCKET'.format(HOST, PORT))
 scope.read_termination = '\n'
 scope.write_termination = '\n'
 scope.timeout = 30000
 
-print('querying scope identification')
+print('getting scope id')
 r = scope.query('*idn?')
 if r.split(',')[1] != 'MSO7034B':
     exit('It\'s dark here. You\'re likely to be eaten by a grue.')
 print(r)
 
-print('setting scope parameters')
+print('initializing scope')
 scope.clear()
 scope.write('*rst')
 scope.write(':timebase:mode main')
 scope.write(':timebase:range 1e-2')
 scope.write(':timebase:delay 0')
 scope.write(':timebase:reference left')
-scope.write(':channel1:probe 10')
-scope.write(':channel1:range 1.6e1')
+scope.write(':channel1:probe 1')
+scope.write(':channel1:range 1.6e0')
 scope.write(':channel1:offset 0')
 scope.write(':channel1:coupling dc')
 scope.write(':trigger:mode edge')
 scope.write(':trigger:holdoff 1e0')
-scope.write(':trigger:level 0.1')
+scope.write(':trigger:level 5.0e-2')
 scope.write(':trigger:hfreject on')
 scope.write(':trigger:reject hf')
 scope.write(':trigger:slope positive')
 scope.write(':trigger:source chan1')
 scope.write(':trigger:sweep normal')
 scope.write(':acquire:type normal')
-scope.write(':acquire:complete 100')
-scope.write(':acquire:count 1')
 scope.write(':digitize channel1')
 
 # digitize will block further interaction with instrument,
 # so generate a pulse, otherwise we will catch a timeout exception
+
 print('generating pulse')
-ssh_cmd = 'ssh cpsb4_wan'
-# exe = '/home/pi/projects/cps/interface/close_one'
-exe = '/home/pi/projects/cps/interface/open_one'
-device = '/dev/ttyAMA0'
-row = '191'
-col = '228'
-cmd = '{} -d {} -row {} -col {}'.format(exe, device, row, col)
-r = proc.run(ssh_cmd.split() + cmd.split())
+proc.run(sys.argv[1].split())
 
 print('gathering waveform data')
 scope.write(':waveform:source channel1')
@@ -83,10 +78,6 @@ scope.write(':waveform:points 1000')
 scope.write(':waveform:format word')
 scope.write(':waveform:unsigned on')
 scope.write(':waveform:byteorder lsbfirst')
-
-# Get X/Y axis ranges
-xrng = float(scope.query(':timebase:range?'))
-yrng = float(scope.query(':channel1:range?'))
 
 # Get axis configuration from preamble (guide p.673)
 r = scope.query(':waveform:preamble?')
@@ -97,18 +88,17 @@ xinc, xorg, xref, yinc, yorg, yref = [float(i) for i in r.split(',')[4:]]
 r = scope.query_binary_values(':waveform:data?', datatype='H')
 acq_data = np.array(r)
 
-volts = (acq_data - yref) * yinc + yorg
-time = np.arange(0, xinc * len(volts), xinc)
+voltage = (acq_data - yref) * yinc + yorg
+time = np.arange(0, xinc * len(voltage), xinc)
 
-print('plotting waveform data')
-plt.ylabel('volts')
-plt.xlabel('time')
-plt.margins(x=0)
-plt.grid(color='#dddddd', linestyle='-', linewidth=1)
-plt.xticks(np.arange(0, xrng, step=xrng/10))
-plt.ylim(-yrng/2, yrng/2)
-plt.plot(time, volts, color='#ff0088') 
-plt.savefig('capture.png', facecolor='w', edgecolor='w', dpi=120)
+filename = 'mso_values.dat'
+print('saving waveform data to {}'.format(filename))
+np.savetxt(filename, np.column_stack((time, voltage)),
+           fmt='%10.6f', delimiter=',', newline='\n')
+        
+fig = tpl.figure()
+fig.plot(time, voltage, label="bumpy snake", width=96, height=24)
+fig.show()
 
 # A waveform record consists of either all of the acquired points or a
 # subset of the acquired points. The number of points acquired may be
